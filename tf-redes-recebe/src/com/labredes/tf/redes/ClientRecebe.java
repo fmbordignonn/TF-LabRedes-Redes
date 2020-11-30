cd023df33618afd01f590ec6e2fccbd5a81a7e84
Felipe Bordignon <felipe.bordignon@edu.pucrs.br>
2020-11-30 01:03:55 -0300

    tratando fast retransmit melhor no server

--- tf-redes-recebe/src/com/labredes/tf/redes/ClientRecebe.java
+++ tf-redes-recebe/src/com/labredes/tf/redes/ClientRecebe.java
@@ -1,13 +1,17 @@
 package com.labredes.tf.redes;
 
-import java.io.*;
 import java.net.*;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 
 class ClientRecebe {
+
+    static Map<Integer, byte[]> receivedFileData = new HashMap<>();
+
+
     public static void main(String args[]) throws Exception {
         System.out.println("Iniciou");
 
@@ -15,8 +19,6 @@ class ClientRecebe {
         DatagramSocket serverSocket = new DatagramSocket(9876);
 
         int lastSeqReceived = -1;
-
-        Map<Integer, byte[]> receivedFileData = new HashMap<>();
 
         byte[] receiveData;
 
@@ -40,7 +42,13 @@ class ClientRecebe {
 
             System.out.println("Mensagem recebida: " + receivedMessage.replace("-", " - "));
 
-            DatagramPacketInfo packetInfo = parseMessage(receivedMessage);
+            DatagramPacketInfo packetInfo = parseInputMessage(receivedMessage);
+
+            int result = checkMissingPackets(packetInfo.getSeq());
+
+            if(!(result == 0)){
+                //APLICAR VALIDAÇOES COM LASTSEQRECEIVED ABAIXO AQUI!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            }
 
             //recebeu pacote de um ack q tava duplicado
 
@@ -49,10 +57,10 @@ class ClientRecebe {
             //foram perdidos no limbo
             //se tiver um faltando, retorna os acks repetidos pra fechar 3 e reenvia,
             //vai fazer isso pra todos os itens que faltarem na lista
-            if(lastSeqReceived < packetInfo.getSeq() - 1 && lastSeqReceived != -1){
+            if (lastSeqReceived < packetInfo.getSeq() - 1 && lastSeqReceived != -1) {
                 int lostPacket = packetInfo.getSeq() - 1;
 
-                System.out.println("FALTOU PACOTE! - SEQ RECEBIDO [" +packetInfo.getSeq() + "] - PACKET QUE FALTA [" + lostPacket + "]");
+                System.out.println("FALTOU PACOTE! - SEQ RECEBIDO [" + packetInfo.getSeq() + "] - PACKET QUE FALTA [" + lostPacket + "]");
 
                 //e se falhar mais de um em seguida?
 
@@ -74,7 +82,7 @@ class ClientRecebe {
             //se o valor da ultima sequencia recebida for diferente da seq do pacote recebido agora -1, significa que faltou algum pacote
             //no caminho.
             //isso é o "fast retransmit"
-            if(lastSeqReceived != packetInfo.getSeq() - 1 && lastSeqReceived != -1){
+            if (lastSeqReceived != packetInfo.getSeq() - 1 && lastSeqReceived != -1) {
                 DatagramPacket response = new DatagramPacket(sendData, sendData.length, IPAddress, port);
 
                 response.setData(("ACK-" + lastSeqReceived).getBytes());
@@ -88,7 +96,7 @@ class ClientRecebe {
 
             //retirando delimiter da msg no client q envia o file (variavel FILE_END_DELIMITER_CHAR)
             for (int i = 0; i < packetInfo.getFileData().length; i++) {
-                if(packetInfo.getFileData()[i] == 124){
+                if (packetInfo.getFileData()[i] == 124) {
                     packetInfo.getFileData()[i] = 0;
                     System.out.println("pacote final");
                 }
@@ -109,7 +117,33 @@ class ClientRecebe {
         }
     }
 
-    public static DatagramPacketInfo parseMessage(String message) {
+    /**
+     * @param seqReceived sequence number recebido no pacote atual
+     * @return 0 se nao há pacotes faltando, senao retorna o sequence number do pacote faltando
+     */
+    public static int checkMissingPackets(int seqReceived) {
+        //pega todos os seqs antes do que chegou agora
+        List<Integer> lista = receivedFileData.keySet()
+                .stream()
+                .filter(seq -> seq > seqReceived)
+                .collect(Collectors.toList());
+
+        //verifica se os ultimos pacotes antes do que chegou agora chegaram ok..
+        //ver se o size nao vai buga
+        for (int seq = 1; seq < lista.size(); seq++) {
+            //se um pacote nao chegou, precisa pedir de novo
+            if (seq != lista.get(seq)) {
+                return seq;//faltou pacote aqui (i tá faltando)
+
+                //se tiver varios faltando, ele vai pedir primeiro um, dps vai enviar o outro.
+                //como mexe direto no dicionario de dados recebidos, ele vai ficar pedindo o mesmo sempre até receber ele
+            }
+        }
+
+        return 0;
+    }
+
+    public static DatagramPacketInfo parseInputMessage(String message) {
         DatagramPacketInfo packetInfo = new DatagramPacketInfo();
 
         String[] splitMessage = message.split("-");
